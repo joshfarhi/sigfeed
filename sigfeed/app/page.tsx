c0ac07977ad90286968fcd5a31a85dce7d888fbd
joshfarhi <joshuafarhi@protonmail.com>
2024-02-21 14:32:31 -0700

    updated

--- sigfeed/app/page.tsx
+++ sigfeed/app/page.tsx
@@ -3,13 +3,23 @@
 import React, { useState, Suspense } from 'react';
 const InstagramCard = React.lazy(() => import('../components/instagramCard'));
 const RedditCard = React.lazy(() => import('../components/redditCard'));
-import NewPost from '@/components/newPost';
+// Updated to use relative import path
+import NewPost from '../components/newPost';
+
+// Define a type for your posts
+interface Post {
+  type: string;
+  url: string;
+  [key: string]: any; // Allows for additional properties of any type
+}
 
 const Home: React.FC = () => {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState<Post[]>([]);
 
-    const handleNewPost = (post) => {
+    const handleNewPost = (post: Post) => {
         setPosts([post, ...posts]); // Prepend new posts to maintain order
+        // Added for debugging: Log the updated posts state
+        console.log(posts);
     };
 
     return (
